2a9da41e7c17f3887b5ac1df72f35390fee83347
Diego Lorden <djlorden@gmail.com>
2013-04-07 20:03:03 +0200

    Initial commit

---
+++ js/digestion.js
@@ -0,0 +1,11 @@
+/*
+$(document).ready(function(){
+    $('.food-group-title').click(function(){
+        if($(this).next().css('display') == 'block'){
+            $(this).next().hide();
+        } else {
+            $(this).next().show();
+        }
+    });
+});
+*/
