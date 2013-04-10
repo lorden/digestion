198fd87c34f711a41ac8c548a8a312dd1e384a43
Diego Lorden <djlorden@gmail.com>
2013-04-10 19:45:38 +0200

    Style and items toggling, images set

--- js/digestion.js
+++ js/digestion.js
@@ -6,16 +6,67 @@ $(document).ready(function(){
         $('#main-container').foggy();
         $('#footer').foggy();
     } else {
-        eraseCookie('digestion');
+        var preset = readCookie("digestion");
+        selectItems(preset);
+        // eraseCookie('digestion');
     }
+
     $('#start').click(function(){
         $('#welcome').modal('hide');
         $('#header').foggy(false);
         $('#main-container').foggy(false);
         $('#footer').foggy(false);
         createCookie("digestion", $('#preset').val(), 90);
+        selectItems($('#preset').val());
+        checkGroup(null);
     });
+
+    $('.food-group-item').click(function(){
+        $(this).toggleClass('removed');
+        checkGroup($(this).parent().parent().attr('id'));
+    });
+    
 });
+
+function selectItems(mode){
+    if(mode == 'veget' || mode == 'vegan'){
+        $('#beef').addClass('removed');
+        $('#poultry').addClass('removed');
+        $('#pork').addClass('removed');
+        $('#fish').addClass('removed');
+        $('#shellfish').addClass('removed');
+    }
+    if(mode == 'vegan'){
+        $('#dairy').addClass('removed');
+        $('#eggs').addClass('removed');
+        $('#honey').addClass('removed');
+    }
+}
+
+function checkGroup(group){
+    if(group !== null){
+        var root = '#' + group;
+    } else {
+        var root = '.food-group';
+    }
+    $(root).each(function(){
+        var total = 0;
+        var removed = 0;
+        $(this).find('.food-group-item').each(function(){
+            if($(this).hasClass('removed')){
+                removed += 1;
+            }
+            total += 1;
+        });
+        if(removed == 0){
+            $(this).append('<img src="/img/check.png" alt="" />');
+        } else if(total === removed){
+            $(this).append('<img src="/img/cross.png" alt="" />');
+        } else {
+        }
+    });
+}
+
 function createCookie(name, value, days) {
     if (days) {
         var date = new Date();
